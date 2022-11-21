a6e9b0801f6d61385421a0ad1c2d5494c6e74185
ana0916 <tonymeine@gmail.com>
2022-11-21 19:11:53 -0300

    iniciado html, css e  js

---
+++ exercicios/ana-sulli/projeto-casa/script.js
@@ -0,0 +1,29 @@
+const input = document.querySelector('#input');
+const imgDigi = document.querySelector('#img-digi');
+const digiTitle = document.querySelector('.card-title');
+const digiText = document.querySelector('card-text')
+
+const fetchAPI = async () => {
+  const response = await fetch('https://digimon-api.vercel.app/api/digimon');
+  const data = await response.json();
+
+  return data;
+}
+
+async function test({ target }) {
+  const digiArr = await fetchAPI();
+
+  const find = digiArr.find(({ name }) => name.toLowerCase() === target.value.toLowerCase());
+  
+  const { name, level, img } = find;
+
+  digiText.innerHTML = `${name} is a ${level} type of digimon`;
+  digiTitle.innerHTML = name;
+  imgDigi.src = img;
+}
+
+function digiAPI() {
+  input.addEventListener('change', test);
+}
+
+window.onload = () => digiAPI();
